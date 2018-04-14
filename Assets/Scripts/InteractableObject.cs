7729d0c36afee29b1c77a4a3ee9b5072478cfe51
Jakob Pærregaard Holm <jakkerph@hotmail.com>
2018-04-14 12:00:33 +0200

    Basic audio and bug fixes

---
+++ Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject : Clickable {
+
+    public AudioClip audioClip;
+    private AudioSource audioSource;
+    private bool play;
+
+    public override void Interact(){
+        //Do something with audioClip
+        Debug.Log("MY NAME: "+this.gameObject.name);
+        clickedOnObject();
+    }
+
+    public void clickedOnObject(){
+        //Do something with audio
+
+        //Mute
+        audioSource.mute = true;
+        play = false;
+    }
+
+    // Use this for initialization
+    void Start () {
+        play = true;
+        audioSource = this.GetComponent<AudioSource>();
+        audioSource.clip = audioClip;
+        StartCoroutine(playSound(2.5f));
+
+    }
+
+    public IEnumerator playSound(float waitTime){
+        if (play){
+            yield return new WaitForSeconds(waitTime);
+            audioSource.Play();
+            StartCoroutine(playSound(waitTime));
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
