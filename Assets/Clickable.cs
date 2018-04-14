71e3d061ec9ff4fe954386bd72ea7bee09ff0f01
Jakob Pærregaard Holm <jakkerph@hotmail.com>
2018-04-14 09:27:11 +0200

    Added functinality to Clickable and started the basic InteractableObject scritp

--- Assets/Clickable.cs
+++ Assets/Clickable.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public abstract class Clickable : MonoBehaviour {
 
+    public Material basic_material;
+    public Material basic_material_with_outline;
+
     public abstract void Interact();
 
     public void Hover(){
+        this.gameObject.GetComponent<Renderer>().material = basic_material_with_outline;
+    }
 
+    public void NoHover(){
+        //var mat = this.gameObject.GetComponent<Material>();
+        //mat = basic_material_with_outline;
+        this.gameObject.GetComponent<Renderer>().material = basic_material;
     }
 
 }
---
+++ Assets/InteractableObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject : Clickable {
+
+    public AudioClip audioClip;
+
+    public override void Interact(){
+        //Do something with audioClip
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
